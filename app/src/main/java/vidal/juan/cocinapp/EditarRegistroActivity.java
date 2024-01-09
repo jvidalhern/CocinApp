28b696bea6865f813bf0d83084c3403a35a87ccf
galvareval <galvareval@educacion.navarra.es>
2024-01-09 16:32:51 +0100

    Prueba comit

---
+++ app/src/main/java/vidal/juan/cocinapp/EditarRegistroActivity.java
@@ -0,0 +1,14 @@
+package vidal.juan.cocinapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditarRegistroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editar_registro);
+    }
+
+}
