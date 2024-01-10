e4cf07ee68e172bf75fe813384f0e115e03d0fbc
galvareval <galvareval@educacion.navarra.es>
2024-01-10 21:07:35 +0100

    inicio: Realizar el pedido una vez seleccionado el menu; investigar restricciones en calendar

--- app/src/main/java/vidal/juan/cocinapp/EditarRegistroActivity.java
+++ app/src/main/java/vidal/juan/cocinapp/EditarRegistroActivity.java
@@ -5,6 +5,9 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Modificar el registro del usuario; una vez este ya esté logeado
+ */
 public class EditarRegistroActivity extends AppCompatActivity {
     private EditText modNombre,modDepartamento,modTelefono;
     @Override
@@ -12,7 +15,7 @@ public class EditarRegistroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editarregistro);
         //rellenar el hint con los datos que ya tiene el usuario
-        //obtenerlo de la bbdd
+        //obtenerlo de la bbdd los datos del usuario logeado; Ctemp: crear usuario temporal ?
     }
 
 }
