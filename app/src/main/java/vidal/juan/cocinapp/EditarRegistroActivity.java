8d0e05067e3055832746752ac03a5f325775ff69
galvareval <galvareval@educacion.navarra.es>
2024-01-18 17:21:55 +0100

    Funciona el login; Funciona mostrar regitros

--- app/src/main/java/vidal/juan/cocinapp/EditarRegistroActivity.java
+++ app/src/main/java/vidal/juan/cocinapp/EditarRegistroActivity.java
@@ -4,65 +4,91 @@ import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
 import android.service.autofill.UserData;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 /**
  * Modificar el registro del usuario; una vez este ya esté logeado
  */
 public class EditarRegistroActivity extends AppCompatActivity {
+    //Items de la vista
     private EditText modNombreEditText,modApellidoEditText,modDepartamentoEditText,modTelefonoEditText;
-    //TODO: Para conectar a la BBDD hay que tener una rereferencia
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference().child("usuarios");
+    //Para manejar los resultados de la consulta
+    private String nombreUserOriginal = "incioVar ";
+    private String apellidosOriginal = " ";
+    private String departamentoOriginal = " ";
+    private String telefonoOriginal = " ";
+    private Usuarios usuarioDatos;
+
+    //Uusario logeado en la app
+    FirebaseUser usuarioLogeado = FirebaseAuth.getInstance().getCurrentUser();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editarregistro);
-        //Ref a los campos
+        //Ref a los campos de la vista
         modNombreEditText = findViewById(R.id.modNombreEditText);
         modApellidoEditText = findViewById(R.id.modApellidoEditText);
         modDepartamentoEditText = findViewById(R.id.modDepartamentoEditText);
         modTelefonoEditText = findViewById(R.id.modTelefonoEditText);
 
+        //Para conectar a la BBDD mediante una referencia
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://cocinaapp-7da53-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference().child("usuarios");
+        //Consulta para conseguir los datos del usuario logedao;filtrar por el mail
+        Query datosUsuarioLogeado = myRef.orderByChild("email").equalTo(usuarioLogeado.getEmail());
+        //"Ejecutar la consulta"
+        obtenerDatosUserLogeado(datosUsuarioLogeado);
+        //Toast.makeText(EditarRegistroActivity.this,nombreUserOriginal, Toast.LENGTH_LONG).show();
+        //Validar la modificacion de datos por parte del usuario
+        Log.d("pruebaaa","origi " + nombreUserOriginal);
+        Log.d("pruebaaa","debbd " + modNombreEditText.getText().toString());
+        verificarModificacionCampo(modNombreEditText,nombreUserOriginal);
 
+    }
 
-        //rellenar el hint con los datos que ya tiene el usuario
-        //obtenerlo de la bbdd los datos del usuario logeado; Ctemp: crear usuario temporal ?
-        //Obtener cambios en los datos del usuario
-
-        ValueEventListener datosUsuarioListener = new ValueEventListener() {
+    /**
+     * Método para obtener los datos personales del usuario logeado
+     * @param datosUsuarioLogeado Query con referencia al email del usuario logeado
+     */
+    public void obtenerDatosUserLogeado(Query datosUsuarioLogeado) {
+        datosUsuarioLogeado.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // obtener objeto con los datos del usuario
-                //UserData user = dataSnapshot.getValue(UserData.class);
-                //TODO: Investigar como filtrar los datos, la conexion con la BBDD ussuario la hace ahora hay que extraer los datos
 
-                // ..
-                //Resultados de la consulta
-                String nombreUser = " ";
-                //modNombreEditText.setText(dataSnapshot.toString());
+                //El resultado se devuelve en un DataSnapshot, hay que recorrer con un FOR aunq solo sea un resultado
                 for (DataSnapshot userSnapshot: dataSnapshot.getChildren())
                 {
-                    Usuarios usuarioDatos = userSnapshot.getValue(Usuarios.class);
-                   // nombreUser = usuarioDatos.getNombre().toString();
-                    //nombreUser = usuarioDatos.toString();
-                    System.out.println("preuba" + usuarioDatos.toString());
-                    Log.d("prueba",usuarioDatos.toString());
-
-
+                    //Crear un objeto usuario a partir de los datos filtrados para el correo del usuario logeado
+                     usuarioDatos = userSnapshot.getValue(Usuarios.class);
+                    //Obtener los datos para mostrarlos en la vista
+                    nombreUserOriginal = usuarioDatos.getNombre();
+                    apellidosOriginal = usuarioDatos.getApellidos();
+                    departamentoOriginal = usuarioDatos.getDepartamento();
+                    telefonoOriginal = usuarioDatos.getTelefono();
                 }
-               // modNombreEditText.setText(nombreUser);
-
+                //Pasar los datos origianles a la vista
+                modNombreEditText.setText(nombreUserOriginal);
+                modApellidoEditText.setText(apellidosOriginal);
+                modDepartamentoEditText.setText(departamentoOriginal);
+                modTelefonoEditText.setText(telefonoOriginal);
+                //Toast.makeText(EditarRegistroActivity.this,nombreUserOriginal, Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -70,9 +96,22 @@ public class EditarRegistroActivity extends AppCompatActivity {
                 // fallo al obtener los datos
                 Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
             }
-        };
-        myRef.addValueEventListener(datosUsuarioListener);
+        });
 
+        nombreUserOriginal = usuarioDatos.getNombre();
+        apellidosOriginal = usuarioDatos.getApellidos();
+        departamentoOriginal = usuarioDatos.getDepartamento();
+        telefonoOriginal = usuarioDatos.getTelefono();
+        Toast.makeText(EditarRegistroActivity.this,nombreUserOriginal, Toast.LENGTH_LONG).show();
+    }
+
+    public void verificarModificacionCampo(EditText campoValidar, String campoOriginal)
+    {
+        if (!campoValidar.getText().equals(campoOriginal))
+        {
+            campoValidar.setError(null);
+        }
+        else campoValidar.setError("");
     }
 
 }
--- app/src/main/java/vidal/juan/cocinapp/Usuarios.java
+++ app/src/main/java/vidal/juan/cocinapp/Usuarios.java
@@ -59,3 +59,4 @@ public class Usuarios {
         this.telefono = telefono;
     }
 }
+
