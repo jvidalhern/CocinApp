c4bfefed752d15513f634578650e2708f89932aa
galvareval <galvareval@educacion.navarra.es>
2024-02-13 19:09:53 +0100

    Cambio el diseño de la vista activity_ver_detalles_pedido.xml y activity_ver_pedido.xml; ahora se ve mejor el diseño. TODO logica modificar el pedido

--- app/src/main/java/vidal/juan/cocinapp/VerPedidoActivity.java
+++ app/src/main/java/vidal/juan/cocinapp/VerPedidoActivity.java
@@ -103,7 +103,7 @@ public class VerPedidoActivity extends AppCompatActivity {
 
                                         //Cargar los datos en los campos
 
-                                        textViewIdPedido.setText("ID pedido: " + String.valueOf(pedidoActivo.getIdPedido()).substring(3,7));
+                                        textViewIdPedido.setText(getString(R.string.idPedidoString) + String.valueOf(pedidoActivo.getIdPedido()).substring(3,7));
                                         textViewFechaPedido.setText(String.valueOf(pedidoActivo.getFecha_pedido()));
                                         textViewFechaEntrega.setText(String.valueOf (pedidoActivo.getFecha_entrega()));
                                         textViewEstado.setText(String.valueOf (pedidoActivo.getEstado()));
