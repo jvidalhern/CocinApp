b6256606194f0e167939edc1bd6a42248573f13b
Gianfranco Álvarez Valencia <galvareval@educacion.navarra.es>
2024-02-21 19:47:04 +0100

    Funcionalidad de añadir y quitar cantidad de registro acutualizando la vista de lista y la vista de modificar pedido

---
+++ app/src/main/java/vidal/juan/cocinapp/Racion.java
@@ -0,0 +1,87 @@
+package vidal.juan.cocinapp;
+
+import java.util.List;
+
+/**
+ * Clase que representa una racion de la BBDD
+ */
+public class Racion {
+
+    private List<String> alergenos;
+    private String descripcion;
+    private String foto;
+    private int pedido_max;
+    private String precio;
+    private String stock;
+    public Racion() {
+    }
+
+    public Racion(List<String> alergenos, String descripcion, String foto, int pedido_max, String precio, String stock) {
+        this.alergenos = alergenos;
+        this.descripcion = descripcion;
+        this.foto = foto;
+        this.pedido_max = pedido_max;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public List<String> getAlergenos() {
+        return alergenos;
+    }
+
+    public void setAlergenos(List<String> alergenos) {
+        this.alergenos = alergenos;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public int getPedido_max() {
+        return pedido_max;
+    }
+
+    public void setPedido_max(int pedido_max) {
+        this.pedido_max = pedido_max;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+    @Override
+    public String toString() {
+        return "Racion{" +
+                "alergenos=" + alergenos +
+                ", descripcion='" + descripcion + '\'' +
+                ", foto='" + foto + '\'' +
+                ", pedido_max=" + pedido_max +
+                ", precio='" + precio + '\'' +
+                ", stock='" + stock + '\'' +
+                '}';
+    }
+
+}
