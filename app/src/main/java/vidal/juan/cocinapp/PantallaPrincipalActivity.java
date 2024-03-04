a4507b716b996f55385d497a1dfe27737b8c6f0e
Gianfranco Álvarez Valencia <galvareval@educacion.navarra.es>
2024-03-04 21:11:05 +0100

    Arregaldo lo de los . y , Ahora peta al intentar elminiar cunado no hay nad, lo elimina pero hace el update tambien y charasea la activiy-->TODO arreglar esto

--- app/src/main/java/vidal/juan/cocinapp/PantallaPrincipalActivity.java
+++ app/src/main/java/vidal/juan/cocinapp/PantallaPrincipalActivity.java
@@ -2,6 +2,7 @@ package vidal.juan.cocinapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.text.DecimalFormatSymbols;
+
 public class PantallaPrincipalActivity extends AppCompatActivity {
     //Items del xml
     TextView usuarioLogeadoTextView;
@@ -19,7 +22,8 @@ public class PantallaPrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
-
+        //ver si usa . o coma todo quitar esto, es una prueba
+        Log.d("getDecimal", "Decimal" +  DecimalFormatSymbols.getInstance().getDecimalSeparator());
         //Ref items del xml
         usuarioLogeadoTextView = findViewById(R.id.usuarioLogeadoTextView);
         editarRegistroButton = findViewById(R.id.editarRegistroButton);
--- app/src/main/java/vidal/juan/cocinapp/SeleccionarRacionesActivity.java
+++ app/src/main/java/vidal/juan/cocinapp/SeleccionarRacionesActivity.java
@@ -22,7 +22,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class SeleccionarRacionesActivity extends AppCompatActivity {
 
@@ -102,9 +104,19 @@ public class SeleccionarRacionesActivity extends AppCompatActivity {
                         String descripcion = childSnapshot.child("descripcion").getValue(String.class);
                         Long pedidoMax = childSnapshot.child("pedido_max").getValue(Long.class);
                         String precioPrev = childSnapshot.child("precio").getValue(String.class);
+                        // Crea una instancia de DecimalFormat con el formato deseado
+                        Log.d("getDecimalPrecio", "Decimal y precio de bbdd" +  DecimalFormatSymbols.getInstance().getDecimalSeparator() + "precio:  " + precioPrev);
+                        Locale locale = Locale.getDefault();
+                        Log.d("Locale", "Idioma: " + locale.getLanguage() + ", País: " + locale.getCountry());
+
                         double precioDecimal = Double.parseDouble(precioPrev);
-                        DecimalFormat df = new DecimalFormat("#.00");
+                        Log.d("getDecimalPrecio", "Precio decimal" +  precioDecimal);
+                        // Crear DecimalFormatSymbols con el punto como separador decimal
+                        DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
+                        symbols.setDecimalSeparator('.');
+                        DecimalFormat df = new DecimalFormat("#.00",symbols);
                         String precio = df.format(precioDecimal);
+                        Log.d("getDecimalPrecio", "Precio decimal depues del format" +  precio);
                         String stock = childSnapshot.child("stock").getValue(String.class);
                         String urlImagen = URL_FOTOS + childSnapshot.child("foto").getValue(String.class) + URL_SUFIJO;
 
